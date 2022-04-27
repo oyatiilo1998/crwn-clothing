1bd9928b7ab769ad0d9718a5da9d4d9480408903
Oyatillo Abdusattorov <oyatilloabdusattorov@gmail.com>
2022-04-27 18:19:15 +0500

    sign in with firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,21 @@
+import { getRedirectResult } from "firebase/auth";
+import { useEffect } from "react";
 import {
+  auth,
   signInWithGooglePopup,
+  singInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    const redirectFunction = async () => {
+      const response = await getRedirectResult(auth);
+      console.log(response);
+    };
+    redirectFunction();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,6 +25,7 @@ const SignIn = () => {
     <div>
       <h1>SignIn</h1>
       <button onClick={logGoogleUser}>Sign in with google</button>
+      <button onClick={singInWithGoogleRedirect}>Sign in with Redirect</button>
     </div>
   );
 };
